3367a33d9815645b16160479d1ace95b98f57b82
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2024-11-15 01:41:37 +0530

    refactor(user-routes): refactors user route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
 const authRoutes = require('./authRoutes');
-const {protectRouteHandler} = require('../controllers/authController');
 
 const router = express.Router();
-router.use('/user', protectRouteHandler, userRoutes);
+router.use('/user', userRoutes);
 router.use('/product', productRoutes);
 router.use('/auth', authRoutes);
 
