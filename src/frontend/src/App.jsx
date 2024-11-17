2da627f188b2e3981ef344a5c12251e6594c25aa
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2024-11-17 18:28:42 +0530

    feat(front-end): adds demo page

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>Frontend Demo</h1>
+            <div className="card">
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Frontend Demo page</a>
+            </div>
+        </>
+    )
+}
+
+export default App
