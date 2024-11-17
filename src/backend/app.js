c763c82fc29dda3f223e750a1ef8af655f996040
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2024-11-17 21:26:12 +0530

    refactor(app.js): removes unused code

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,13 +5,10 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const apiRoutes = require("./routes/index");
 const cors = require("cors");
-const Razorpay = require("razorpay");
-const shortId = require("shortid");
-const crypto = require("crypto");
 
 dotenv.config();
 
-const { PORT, MONGODB_URI, RAZRPAY_PUBLIC_KEY, RAZRPAY_PRIVATE_KEY, WEBHOOK_SECERET } = process.env;
+const { PORT, MONGODB_URI} = process.env;
 const app = express();
 
 // mongodb configuration
@@ -27,57 +24,6 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-const razorpayInstance = new Razorpay({
-  key_id: RAZRPAY_PUBLIC_KEY,
-  key_secret: RAZRPAY_PRIVATE_KEY,
-});
-
-// initial booking
-app.post("/checkout", async function (req, res, next) {
-  const amount = 500;
-  const currency = "INR";
-  const payment_capture = 1;
-  try {
-    const options = {
-      amount: amount * 100,
-      currency: currency,
-      receipt: shortId.generate(),
-      payment_capture: payment_capture
-    }
-    const orderObject = await razorpayInstance.orders.create(options);
-    // send to frontend 
-    res.status(200).json({
-      status: "success",
-      message: {
-        id: orderObject.id,
-        currency: orderObject.currency,
-        amount: orderObject.amount
-      }
-    })
-  } catch (err) {
-    next(err);
-  }
-})
-
-// payment verification
-app.post("/verification", async function (req, res, next) {
-  try {
-    // this object -> sha256+webhook_secret
-    const shasum = crypto.createHmac("sha256", WEBHOOK_SECERET);
-    shasum.update(JSON.stringify(req.body));
-    const freshSignature = shasum.digest("hex");
-    const razorPaySign = req.headers["x-razorpay-signature"];
-    if (freshSignature === razorPaySign) {
-      res.status(200).json({
-        message: "OK",
-      });
-    } else {
-      throw createError.Forbidden('Invalid Payment.')
-    }
-  } catch (err) {
-    next(err);
-  }
-})
 
 // api routes
 app.use("/api", apiRoutes);
