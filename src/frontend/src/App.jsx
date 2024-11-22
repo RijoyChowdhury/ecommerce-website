e7254641ff4c37cf1aa1b192b3e63d304a8192f6
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2024-11-22 21:30:12 +0530

    feat(front-end): adds placeholder routes

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,16 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './views/Home';
+import Cart from "./views/Cart";
+import User from './views/User';
+import Signup from "./views/Signup";
+import Login from "./views/Login";
+import ProductDetails from './views/ProductDetails';
+import PageNotFound from './views/PageNotFound';
 import './App.css';
 
 function App() {
     return (
-        <>
-            <h1>Frontend Demo</h1>
-            <div className="card">
-                <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >Frontend Demo page</a>
-            </div>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart"  element={<Cart />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Login />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
     )
 }
 
