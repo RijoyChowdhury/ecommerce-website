295beedd3f7e9abf2beb56658a3adb673df32d26
Rijoy Chowdhury <chowdhuryrijoy@gmail.com>
2025-01-03 14:22:06 +0530

    feat(routes): adds cart routes

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -4,6 +4,7 @@ const productRoutes = require('./productRoutes');
 const authRoutes = require('./authRoutes');
 const bookingRoutes = require('./bookingRoutes');
 const reviewRoutes = require('./reviewRoutes');
+const cartRoutes = require('./cartRoutes');
 
 const router = express.Router();
 router.use('/user', userRoutes);
@@ -11,6 +12,7 @@ router.use('/product', productRoutes);
 router.use('/auth', authRoutes);
 router.use('/booking', bookingRoutes);
 router.use('/review', reviewRoutes);
+router.use('/cart', cartRoutes);
 
 
 module.exports = router;
